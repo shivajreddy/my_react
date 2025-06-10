9cbb595fcdd9dd69546634cf8a18ee9afae7facf
shiva <sreddy@hey.com>
2025-06-10 12:29:20 -0400

    .

--- pilot/src/App.tsx
+++ pilot/src/App.tsx
@@ -1,12 +1,41 @@
-import './App.css'
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
-function App() {
-
+export function App() {
     return (
-        <>
-            <p className='text-md text-red-500'>PILOT</p>
-        </>
-    )
+        <div>
+            <Card className="w-full max-w-sm">
+                <CardHeader>
+                    <CardTitle>Login to your account</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+                    <CardAction>
+                        <Button>Sign Up</Button>
+                    </CardAction>
+                </CardHeader>
+                <CardContent>
+                    <p>hi</p>
+                </CardContent>
+                <CardFooter className="flex-col gap-2">
+                    <Button type="submit" className="w-full">
+                        Login
+                    </Button>
+                    <Button variant="outline" className="w-full">
+                        Login with Google
+                    </Button>
+                </CardFooter>
+            </Card>
+            )
 }
 
-export default App
